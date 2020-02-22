5d0f6deefd98b90cb6bb5f43c4abb0a6e15742e0
Chris Boddy <chrisboddy@Chriss-iMac.local>
2020-02-22 19:54:34 +0000

    removed axios call in ajax.js and added basic search functionality

--- js/ajax.js
+++ js/ajax.js
@@ -1,47 +1,43 @@
-// const axios = require('axios');
-
-// const url = 'https://comicvine.gamespot.com/api/volumes/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21'
-// axios.get(url).then(response => console.log(response));
-
-fetch('make_request.php?searchterm=2000AD').then(function(response) {
-	return response.json();
-}).then(function(json) {
-    const comics = json;
-    console.log(comics);
-	// console.log(comics.results[0].id);
-
-	comics.results.forEach(function(comic){
-		// console.log(`Title ${comic.name} Description ${comic.description} Image ${comic.image.meduim_url}`);		
-	})
-
-	const comicsFragment = document.createDocumentFragment(); //create a fragment
-	comics.results.forEach(function(comic){
-
-		const newTitle = document.createElement("h2");
-		const newTitleText = document.createTextNode(`${comic.name}`);
-
-		const newDiv = document.createElement("div");			
-		const newImage = document.createElement("img");
-
-		newTitle.appendChild(newTitleText);
-		newDiv.innerHTML = `${comic.description}`;
-		newImage.src = `${comic.image.small_url}`;		
-
-		comicsFragment.appendChild(newTitle); 
-		comicsFragment.appendChild(newDiv);
-		comicsFragment.appendChild(newImage);
-	});
-
-	const comicsDiv = document.querySelector("#comics"); //get hold of the div from the page
-	comicsDiv.appendChild(comicsFragment); // we only update the document once!
-	
-});
-
-const txtBox = document.querySelector("#search"); //get hold of the text box
 const srchBtn = document.querySelector(".searchBtn");
 
 srchBtn.addEventListener('click', function (event) {
-	event.preventDefault();		
+	event.preventDefault();	
+
+	const txtBox = document.querySelector("#search"); //get hold of the text box	
  	console.log('The button was clicked!');
-  	console.log(txtBox.value); //displays whatever the user has entered into txtBox.
+	console.log(txtBox.value); //displays whatever the user has entered into txtBox.
+	  
+	fetch(`make_request.php?searchterm=${txtBox.value}`).then(function(response) {
+		return response.json();
+	}).then(function(json) {
+		const comics = json;
+		console.log(comics);
+		console.log(txtBox.value);
+	
+		comics.results.forEach(function(comic){
+			// console.log(`Title ${comic.name} Description ${comic.description} Image ${comic.image.meduim_url}`);		
+		})
+	
+		const comicsFragment = document.createDocumentFragment(); 
+		comics.results.forEach(function(comic){
+	
+			const newTitle = document.createElement("h2");
+			const newTitleText = document.createTextNode(`${comic.name}`);
+	
+			const newDiv = document.createElement("div");			
+			const newImage = document.createElement("img");
+	
+			newTitle.appendChild(newTitleText);
+			newDiv.innerHTML = `${comic.description}`;
+			newImage.src = `${comic.image.small_url}`;		
+	
+			comicsFragment.appendChild(newTitle); 
+			comicsFragment.appendChild(newDiv);
+			comicsFragment.appendChild(newImage);
+		});
+	
+		const comicsDiv = document.querySelector("#comics"); 
+		comicsDiv.appendChild(comicsFragment); 
+		
+	});
 });
--- make_request.php
+++ make_request.php
@@ -1,6 +1,7 @@
 <?php
    $searchTerm =  $_GET["searchterm"];
-$url = 'https://comicvine.gamespot.com/api/search/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21&format=json&sort=name:asc&resources=issue&query='.$searchTerm;
+   $searchOption = $_GET["searchoption"];
+$url = 'https://comicvine.gamespot.com/api/search/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21&format=json&sort=name:asc&resources='.$searchOption.'&query='.$searchTerm;
 $curl_handle=curl_init();
   // curl_setopt($curl_handle,CURLOPT_URL,'https://comicvine.gamespot.com/api/volumes/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21');
   //curl_setopt($curl_handle,CURLOPT_URL, 'https://comicvine.gamespot.com/api/search/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21&format=json&sort=name:asc&resources=issue&query=2000AD');
