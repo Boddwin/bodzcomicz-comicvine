a557a5af8183ff09a3ec71ba0257c7484cee8b74
Chris Boddy <chrisboddy@Chriss-iMac.local>
2020-02-22 14:39:09 +0000

    added scss file and axios node module

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,9 @@
-fetch('make_request.php').then(function(response) {
+// const axios = require('axios');
+
+// const url = 'https://comicvine.gamespot.com/api/volumes/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21'
+// axios.get(url).then(response => console.log(response));
+
+fetch('make_request.php?searchterm=2000AD').then(function(response) {
 	return response.json();
 }).then(function(json) {
     const comics = json;
@@ -6,7 +11,7 @@ fetch('make_request.php').then(function(response) {
 	// console.log(comics.results[0].id);
 
 	comics.results.forEach(function(comic){
-		console.log(`Title ${comic.name} Description ${comic.description} Image ${comic.image.meduim_url}`);		
+		// console.log(`Title ${comic.name} Description ${comic.description} Image ${comic.image.meduim_url}`);		
 	})
 
 	const comicsFragment = document.createDocumentFragment(); //create a fragment
@@ -32,10 +37,11 @@ fetch('make_request.php').then(function(response) {
 	
 });
 
-const txtBox = document.querySelector("#srch-txt-box"); //get hold of the text box
+const txtBox = document.querySelector("#search"); //get hold of the text box
 const srchBtn = document.querySelector(".searchBtn");
 
-srchBtn.addEventListener('click', function () {
-  console.log('The button was clicked!');
-  console.log(txtBox.value); //displays whatever the user has entered into txtBox.
+srchBtn.addEventListener('click', function (event) {
+	event.preventDefault();		
+ 	console.log('The button was clicked!');
+  	console.log(txtBox.value); //displays whatever the user has entered into txtBox.
 });
--- make_request.php
+++ make_request.php
@@ -1,8 +1,11 @@
 <?php
-    
+   $searchTerm =  $_GET["searchterm"];
+$url = 'https://comicvine.gamespot.com/api/search/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21&format=json&sort=name:asc&resources=issue&query='.$searchTerm;
 $curl_handle=curl_init();
   // curl_setopt($curl_handle,CURLOPT_URL,'https://comicvine.gamespot.com/api/volumes/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21');
-  curl_setopt($curl_handle,CURLOPT_URL, 'https://comicvine.gamespot.com/api/search/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21&format=json&sort=name:asc&resources=issue&query=2000AD');
+  //curl_setopt($curl_handle,CURLOPT_URL, 'https://comicvine.gamespot.com/api/search/?api_key=d51adb6cf15fee230e5015b8933588ed3f96fa21&format=json&sort=name:asc&resources=issue&query=2000AD');
+  curl_setopt($curl_handle,CURLOPT_URL, $url);
+  
   curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
   curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
   $result = curl_exec($curl_handle);
