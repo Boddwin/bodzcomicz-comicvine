609214bf4a7290d9508cb03e026fe8ac6eb0d9d3
Chris Boddy <chrisboddy@Chriss-iMac.local>
2020-03-25 20:29:25 +0000

    Added rule to change heading on different resource type

--- js/ajax.js
+++ js/ajax.js
@@ -59,24 +59,80 @@ srchBtn.addEventListener('click', function (event) {
 				const comicsDiv = document.querySelector("#results"); 
 				comicsDiv.appendChild(selectedComicsFragment); 
 
+				document.addEventListener("DOMContentLoaded", function() {
+					var lazyImages = [].slice.call(document.querySelectorAll("js-lazy-load-image"));
+				  
+					if ("IntersectionObserver" in window) {
+					  let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+						entries.forEach(function(entry) {
+						  if (entry.isIntersecting) {
+							let lazyImage = entry.target;
+							lazyImage.src = lazyImage.dataset.src;
+							lazyImage.srcset = lazyImage.dataset.srcset;
+							lazyImage.classList.remove("lazy");
+							lazyImageObserver.unobserve(lazyImage);
+						  }
+						});
+					  });
+				  
+					  lazyImages.forEach(function(lazyImage) {
+						lazyImageObserver.observe(lazyImage);
+					  });
+					} else {
+					  // Possibly fall back to a more compatible method here
+					}
+				  });
+
 			  }
 		  }
 
 		const comicsFragment = document.createDocumentFragment(); 
 		comics.results.forEach(function(comic){
+
+			const newTitle = document.createElement('h2');
+			
+			const resource = comic.resource_type;							
 	
-			const newTitle = document.createElement("h2");
-			const newTitleText = document.createTextNode(`${comic.name}`);
+			if (comic.name !== null){		
+				const newTitleText = document.createTextNode(`${comic.name}`);		
+				newTitle.setAttribute('class', 'result-title');			
+				newTitle.appendChild(newTitleText);
+				comicsFragment.appendChild(newTitle);	
+			}	else if (comic.issue_number !== null){
+				const newTitleText = document.createTextNode(`${comic.volume.name}: Issue ${comic.issue_number}`);		
+				newTitle.setAttribute('class', 'result-title-issue');			
+				newTitle.appendChild(newTitleText);
+				comicsFragment.appendChild(newTitle);
+			};
 
-			const newImage = document.createElement("img");
+			const newImage = document.createElement('img');
+			newImage.src = `${comic.image.small_url}`;
+			comicsFragment.appendChild(newImage);	
 
-			newTitle.setAttribute('class', 'result-title');
-			newTitle.appendChild(newTitleText);
+			const resourceType = document.createElement('p');			
+			const resourceToUpperCase = resource.replace(/^\w/, c => c.toUpperCase());
+			const resourceTypeText = document.createTextNode(`Resource type: ${resourceToUpperCase}`);
+			resourceType.appendChild(resourceTypeText);
+			comicsFragment.appendChild(resourceType);
 
-			newImage.src = `${comic.image.small_url}`;		
-	
-			comicsFragment.appendChild(newTitle);
-			comicsFragment.appendChild(newImage);
+			if (resource == 'character'){
+				const issueAppearances = document.createElement('p');
+				const issueAppearancesText = document.createTextNode(`${comic.count_of_issue_appearances} Issue appearances`);
+				issueAppearances.appendChild(issueAppearancesText);
+				comicsFragment.appendChild(issueAppearances);
+			};
+
+			if (resource != 'issue'){
+				const publisher = document.createElement('p');
+				const publisherText = document.createTextNode(`Publisher: ${comic.publisher.name}`);
+				publisher.appendChild(publisherText);
+				comicsFragment.appendChild(publisher);
+			};
+			
+			const newDeck = document.createElement('p');
+			const newDeckText = document.createTextNode(`${comic.deck}`);
+			newDeck.appendChild(newDeckText);
+			comicsFragment.appendChild(newDeck);	
 
 			newTitle.addEventListener("click",getShowMsgFnc(comic),false); // add an event listener
 		});
